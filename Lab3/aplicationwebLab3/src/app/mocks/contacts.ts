b0d488a958a8f742527d3978c5a543d7d17ea735
FERNANDO NANDO CHACON MEDINA <marco.chacon@unitec.edu>
2020-11-15 23:05:39 -0600

    delete contacts

--- Lab3/aplicationwebLab3/src/app/mocks/contacts.ts
+++
@@ -1,8 +0,0 @@
-import { Contact } from './../models/contacts';
-
-export const allContacts : Array<Contact> = [
-    { id: 1, title: "Mr", name: "Lewis Hamilton", email: "lewisone@gmail.com", phone: "323-365-3454"  },
-    { id: 2, title: "Mr", name: "Niki Lauda", email: "nikki@gmail.com", phone: "323-234-8924"  },
-    { id: 3, title: "Mr", name: "Sebastian Vettel", email: "vettelFerrari@gmail.com", phone: "323-984-3242"  },
-    { id: 4, title: "Mr", name: "Carlos Sainz", email: "SainzGt@gmail.com", phone: "892-767-3453"  }
-]
